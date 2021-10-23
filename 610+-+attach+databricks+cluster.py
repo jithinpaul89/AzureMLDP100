7d1dfc72bcb888cf862275defb0198fc6e27d6cd
jithinpaul <jithinpaul89@gmail.com>
2021-10-23 23:02:39 +0530

    changes to the acces token

--- 610+-+attach+databricks+cluster.py
+++ 610+-+attach+databricks+cluster.py
@@ -3,40 +3,49 @@
 # as an Attached Compute
 # ------------------------------------------------------
 
+import os
+import azureml.core
 from azureml.core import Workspace
 from azureml.core.compute import DatabricksCompute, ComputeTarget
+from azureml.exceptions import ComputeTargetException
+
+# Check core SDK version number
+print("SDK version:", azureml.core.VERSION)
 
 # Access the Workspace
 print("Accessing the AzureML workspace...")
 ws = Workspace.from_config("./config")
+print(ws.name, ws.resource_group, ws.location, ws.subscription_id, sep = '\n')
 
 
 # Create the configuration information of the cluster
 print("Initializing the parameters...")
-db_resource_group     = "databricksRG001"
+db_resource_group     = "mlops-RG"
 db_workspace_name     = "databricksWS001"
-db_access_token       = "dapi5531203108cb669f04b8f5c33bb3261a-3"
+db_access_token       = "***************************"
 db_compute_name       = "mydbcluster001"
 
+db_compute_name=os.getenv("DATABRICKS_COMPUTE_NAME", db_compute_name) # Databricks compute name
+db_resource_group=os.getenv("DATABRICKS_RESOURCE_GROUP", db_resource_group) # Databricks resource group
+db_workspace_name=os.getenv("DATABRICKS_WORKSPACE_NAME", db_workspace_name) # Databricks workspace name
+db_access_token=os.getenv("DATABRICKS_ACCESS_TOKEN", db_access_token) # Databricks access token
 
-if db_compute_name not in ws.compute_targets:
-    print("Creating Configuration for the DB Cluster....")
-    attach_config = DatabricksCompute.attach_configuration(
-                                resource_group = db_resource_group,
-                                workspace_name = db_workspace_name,
-                                access_token = db_access_token)
-    
-    print("Attaching the compute target....")
-    db_cluster = ComputeTarget.attach(ws, 
-                                      db_compute_name, 
-                                      attach_config)
-    
-    db_cluster.wait_for_completion(True)
-
-else:
-    print("Compute target exists...")
-    db_cluster = ws.compute_targets[db_compute_name]
-
+try:
+    databricks_compute = DatabricksCompute(workspace=ws, name=db_compute_name)
+    print('Compute target {} already exists'.format(db_compute_name))
+except ComputeTargetException:
+    print('Compute not found, will use below parameters to attach new one')
+    print('db_compute_name {}'.format(db_compute_name))
+    print('db_resource_group {}'.format(db_resource_group))
+    print('db_workspace_name {}'.format(db_workspace_name))
+    print('db_access_token {}'.format(db_access_token))
+ 
+    config = DatabricksCompute.attach_configuration(
+        resource_group = db_resource_group,
+        workspace_name = db_workspace_name,
+        access_token= db_access_token)
+    databricks_compute=ComputeTarget.attach(ws, db_compute_name, config)
+    databricks_compute.wait_for_completion(True)
 
 
 
